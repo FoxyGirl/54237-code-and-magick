e59b27f1f68e97feb656c7f4908de70ac54160d0
Elena Pavlova <lenlho22@gmail.com>
2017-01-28 21:46:57 +0300

    Create new callback functions for color changing

--- js/setup.js
+++ js/setup.js
@@ -7,12 +7,34 @@ var setupModalNode = document.querySelector('.setup');
 var setupOpenNode = document.querySelector('.setup-open');
 var setupCloseNode = document.querySelector('.setup-close');
 var wizardCoatNode = document.getElementById('wizard-coat');
-var wizardCoatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)',
-  'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var wizardEyesNode = document.getElementById('wizard-eyes');
-var wizardEyesColors = ['red', 'blue', 'yellow', 'green', 'black'];
 var fireballSetupNode = document.querySelector('.setup-fireball-wrap');
-var fireballSetupColors = ['#30a8ee', '#5ce6c0', '#e848d5', '#e6e848', '#ee4830'];
+var wizardCoatColors = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
+var wizardEyesColors = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+];
+var fireballSetupColors = [
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848'
+];
+
+var changeWizardCoatColor = changeStyle(wizardCoatNode, 'fill', wizardCoatColors);
+var changeWizardEyesColor = changeStyle(wizardEyesNode, 'fill', wizardEyesColors);
+var changeFireballColor = changeStyle(fireballSetupNode, 'background', fireballSetupColors);
 
 setupOpenNode.addEventListener('click', function () {
   setupModalNode.classList.remove('invisible');
@@ -22,48 +44,28 @@ setupCloseNode.addEventListener('click', function () {
   setupModalNode.classList.add('invisible');
 });
 
-wizardCoatNode.addEventListener('click', function (event) {
-  changeStyle(event.currentTarget, 'fill', wizardCoatColors);
-});
+wizardCoatNode.addEventListener('click', changeWizardCoatColor);
 
-wizardEyesNode.addEventListener('click', function (event) {
-  changeStyle(event.currentTarget, 'fill', wizardEyesColors);
-});
+wizardEyesNode.addEventListener('click', changeWizardEyesColor);
 
-fireballSetupNode.addEventListener('click', function (event) {
-  changeStyle(event.currentTarget, 'background', fireballSetupColors);
-});
+fireballSetupNode.addEventListener('click', changeFireballColor);
 
-/** Change style property in element
- * @param {Element} elem : element for changing style property
- * @param {String} styleProp : changeable property
- * @param {Array} arrProp : array of property values
+
+/** Change style property in element from array
+ *
+ * @param {Element} elem - The element for changing style property
+ * @param {string} styleProp - The changeable property.
+ * @param {Array} arrProp - The array of property values
+ * @return function for callback
  */
 function changeStyle(elem, styleProp, arrProp) {
-  var indexProp;
-  var currentProp = elem.style[styleProp];
-  if (styleProp === 'background') {
-    if (currentProp) {
-      currentProp = convertRGBDecimalToHex(currentProp);
-    } else {
-      console.log('!!!' + currentProp); // eslint-disable-line
-    }
-  }
-  indexProp = arrProp.indexOf(currentProp);
-  elem.style[styleProp] = ((indexProp === -1) || (indexProp + 1) === arrProp.length) ?
-    arrProp[0] : arrProp[indexProp + 1];
+  var counter = 0;
+  var element = elem;
+  var styleChangeProp = styleProp;
+  var arrChangeProp = arrProp;
 
-  return;
-}
-
-/** Convert RGB decimal to Hex
- * @param {String} rgb : RGB decimal
- * @return {String} : Hex
- */
-function convertRGBDecimalToHex(rgb) {
-  var values = rgb.match(/\d+/g);
-
-  return '#' + parseInt(values[0]).toString(16) +
-    parseInt(values[1]).toString(16) +
-    parseInt(values[2]).toString(16);
+  return function () {
+    counter = (counter < (arrChangeProp.length - 1)) ? (counter + 1) : 0;
+    element.style[styleChangeProp] = arrChangeProp[counter];
+  };
 }
