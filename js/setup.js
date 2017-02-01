501e180f88d0720eff6be438b5884dc10abb375f
Elena Pavlova <lenlho22@gmail.com>
2017-02-01 23:55:20 +0300

    Add aria role, tabindex and other aria attributes.
    Create keydown listeners and show/hide setupModal functions

--- js/setup.js
+++ js/setup.js
@@ -37,20 +37,40 @@ var changeWizardEyesColorHandler = changeStyle(wizardEyesNode, 'fill', wizardEye
 var changeFireballColorHandler = changeStyle(fireballSetupNode, 'background', fireballSetupColors);
 
 setupOpenNode.addEventListener('click', function () {
+  showSetupModal();
+});
+
+setupOpenNode.addEventListener('keydown', function (event) {
+  if (event.keyCode === 13) {
+    showSetupModal();
+  }
+});
+
+setupCloseNode.addEventListener('click', function () {
+  hideSetupModal();
+});
+
+setupOpenNode.addEventListener('keydown', function (event) {
+  if (event.keyCode === 27) {
+    hideSetupModal();
+  }
+});
+
+function showSetupModal() {
   wizardCoatNode.addEventListener('click', changeWizardCoatColorHandler);
   wizardEyesNode.addEventListener('click', changeWizardEyesColorHandler);
   fireballSetupNode.addEventListener('click', changeFireballColorHandler);
 
   setupModalNode.classList.remove('invisible');
-});
+}
 
-setupCloseNode.addEventListener('click', function () {
+function hideSetupModal() {
   setupModalNode.classList.add('invisible');
 
   wizardCoatNode.removeEventListener('click', changeWizardCoatColorHandler);
   wizardEyesNode.removeEventListener('click', changeWizardEyesColorHandler);
   fireballSetupNode.removeEventListener('click', changeFireballColorHandler);
-});
+}
 
 
 /** Change style property in element from array
