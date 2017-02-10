ce98cb48951185a32695503a4bac9f3f33d85069
Elena Pavlova <lenlho22@gmail.com>
2017-02-10 21:18:15 +0300

    Add getRandomElementExcept method

--- js/utils.js
+++ js/utils.js
@@ -7,5 +7,12 @@ window.utils = {
   getRandomElement: function (array) {
     var randomElementIndex = Math.floor(Math.random() * array.length);
     return array[randomElementIndex];
+  },
+  getRandomElementExcept: function (array, item) {
+    var newColor = null;
+    do {
+      newColor = array[this.getRandomElement(array)];
+    } while (newColor === item);
+    return newColor;
   }
 };
