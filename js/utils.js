6b336ae42a41f7f8e029e7244ffb92060bc67b46
Elena Pavlova <lenlho22@gmail.com>
2017-02-11 14:06:09 +0300

    Incapsulate window.utils

--- js/utils.js
+++ js/utils.js
@@ -6,29 +6,31 @@
 /**
  * @module utils
  * */
-window.utils = {
+window.utils = (function () {
   /**
    * Generate random element form array.
    *
    * @param {Array} array - The array of elements.
    * @return {string} The random element form array.
    */
-  getRandomElement: function (array) {
+  var getRandomElement = function (array) {
     var randomElementIndex = Math.floor(Math.random() * array.length);
     return array[randomElementIndex];
-  },
-  /**
-   * Generate random element form array excepting item.
-   *
-   * @param {Array} array - The array of elements.
-   * @param {string} item - The item for exception.
-   * @return {string} The random element form array excepting item.
-   */
-  getRandomElementExcept: function (array, item) {
-    var newColor = null;
-    do {
-      newColor = this.getRandomElement(array);
-    } while (newColor === item);
-    return newColor;
-  }
-};
+  };
+  return {
+    /**
+     * Generate random element form array excepting item.
+     *
+     * @param {Array} array - The array of elements.
+     * @param {string} item - The item for exception.
+     * @return {string} The random element form array excepting item.
+     */
+    getRandomElementExcept: function (array, item) {
+      var newColor = null;
+      do {
+        newColor = getRandomElement(array);
+      } while (newColor === item);
+      return newColor;
+    }
+  };
+})();
