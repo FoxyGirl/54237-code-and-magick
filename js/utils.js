a96f0f130778c1814b1f8fccfcc68c639304ce86
Elena Pavlova <lenlho22@gmail.com>
2017-02-12 19:07:00 +0300

    Correct window.utils

--- js/utils.js
+++ js/utils.js
@@ -8,29 +8,33 @@
  * */
 window.utils = (function () {
   /**
+   * @private
    * Generate random element form array.
    *
    * @param {Array} array - The array of elements.
    * @return {string} The random element form array.
    */
-  var getRandomElement = function (array) {
+  function getRandomElement(array) {
     var randomElementIndex = Math.floor(Math.random() * array.length);
     return array[randomElementIndex];
-  };
+  }
+
+  /**
+   * Generate random element form array excepting item.
+   *
+   * @param {Array} array - The array of elements.
+   * @param {string} item - The item for exception.
+   * @return {string} The random element form array excepting item.
+   */
+  function getRandomElementExcept(array, item) {
+    var newColor = null;
+    do {
+      newColor = getRandomElement(array);
+    } while (newColor === item);
+    return newColor;
+  }
+
   return {
-    /**
-     * Generate random element form array excepting item.
-     *
-     * @param {Array} array - The array of elements.
-     * @param {string} item - The item for exception.
-     * @return {string} The random element form array excepting item.
-     */
-    getRandomElementExcept: function (array, item) {
-      var newColor = null;
-      do {
-        newColor = getRandomElement(array);
-      } while (newColor === item);
-      return newColor;
-    }
+    getRandomElementExcept: getRandomElementExcept
   };
 })();
